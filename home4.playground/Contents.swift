ef267d09e2003b1c6d78dff9b167412e4b94b325
ritaslivets <rita4kasliva2007@gmail.com>
2026-04-29 23:27:43 +0300

    исправленное дз

--- home4.playground/Contents.swift
+++ home4.playground/Contents.swift
@@ -1,25 +1,25 @@
 //MARK: - 1
 // TODO: - Нужен пробел перед фигурнйо скобкой
-func greeting(){
+func greeting() {
     print("Hello, World!")
 }
 greeting()
 
 // TODO: - Нужен пробел перед фигурнйо скобкой
-func printHi(_ name: String){
+func printHi(_ name: String) {
     print(" hi, \(name)")
 }
 printHi("Misha!")
 
 // TODO: - Нужен пробел перед фигурнйо скобкой
-func printHello(_ name: String) -> String{
+func printHello(_ name: String) -> String {
     "Helo, \(name)!"
 }
 print(printHello("Misha"))
 
 //MARK: - 2
 // TODO: - НЕ Нужен пробел после обычной скобки
-func sumOfSymbols( first: String, second: String) -> Int {
+func sumOfSymbols(first: String, second: String) -> Int {
     var sum = first.count + second.count
     return sum
 }
@@ -29,9 +29,9 @@ print(sumOfSymbols(first: first, second: second))
 
 //MARK: - 3
 // TODO: - Пробелы между -> и перед фигурной скобкой
-func squareOfNumber(_ number: Int)->Int{
+func squareOfNumber(_ number: Int) -> Int {
     // TODO: - Пробелы между арифмитескими знаками
-    let square = number*number
+    let square = number * number
     return square
 }
 let number = 13
@@ -39,21 +39,21 @@ print(squareOfNumber(number))
 
 //MARK: - 4
 // TODO: - Пробелы. И навзщние фцнкции должно содержать глагол.
-func areaOfCircle(radius: Double)->Double{
+func countAreaOfCircle(radius: Double) -> Double {
     // TODO: - Навзания пермеенных всегда с маленькой буквы
-    let Pi = 3.14
-    let area = Pi*radius*radius
+    let pi = 3.14
+    let area = pi * radius * radius
     return area
 }
 let radius: Double = 12.5
-print(areaOfCircle(radius: radius))
+print(countAreaOfCircle(radius: radius))
 
 //MARK: - 5
 // TODO: - Пробел и глагол. Если ввести число 23 то выведет некорректно условие. Ну и по хорошему немного бы поменять условие в else if. Он овключает и промежутки из первого условия. По хорошему должны быть конкртено только те, которые отвечают за ночь.
-func dayOrNight(time: Int){
-    if time >= 6 && time < 22{
+func printDayOrNight(time: Int) {
+    if time >= 6 && time < 22 {
         print("Сейчас день!")
-    } else if time <= 22 && time >= 0 || time == 24 {
+    } else if (time >= 22 && time <= 24) || (time >= 0 && time < 6)  {
         print("Сейчас ночь!")
     } else {
         print("неккоректно введенно время!")
@@ -66,14 +66,14 @@ func dayOrNight(time: Int){
       //  print("Сейчас ночь!")
    // }
 }
-dayOrNight(time: 6)
+printDayOrNight(time: 23)
 
 //MARK: - 6
 /* TODO: - Глагол. 
     Не ошибка, но можно делать без переменной period. А сразу использовать return внутри блоков if else if
     Попробуй решить еще задачу с помощью switch case.
 */
-func timeOfYear(month: Int) -> String {
+func printTimeOfYear(month: Int) -> String {
     let period: String
     if month >= 1 && month <= 2 || month == 12 {
         period = " winter"
@@ -84,26 +84,37 @@ func timeOfYear(month: Int) -> String {
     } else if month >= 9 && month <= 11 {
         period = " fall "
     } else {
-        period = " неккоректно введенно время!"
+        period = "неккоректно введенно время!"
     }
     return period
 }
-print(timeOfYear(month: 8))
+print(printTimeOfYear(month: 8))
+//switch
+func printSeason(month: Int) {
+    switch month {
+    case 12,1,2 : print("winter")
+    case 3,4,5 : print("spring")
+    case 6,7,8 : print("summer")
+    case 9,10,11 : print("fall")
+    default : print("неккоректно введенно время!")
+    }
+}
+printSeason(month: 9)
 
  // TODO: - Нейминг переменных
 //MARK: - 7
 func phoneNumber( number: String ){
     // TODO: - let codeOfCountry или даже лучше let countryCode
-    let kodeofCountry = String(number.prefix(3))
-    print(" код страны: \(kodeofCountry)")
+    let countryCode = String(number.prefix(3))
+    print(" код страны: \(countryCode)")
      // TODO: - let phoneWithoutCode
-    let afterremain = String(number.dropFirst(3))
+    let phoneWithoutCode = String(number.dropFirst(3))
     // TODO: - let operatorCode
-    let kodeOfOperator = String(afterremain.prefix(2))
-    print(" код оператора: \(kodeOfOperator)")
+    let operatorCode = String(phoneWithoutCode.prefix(2))
+    print(" код оператора: \(operatorCode)")
      // TODO: - let phoneNumber
-    let phnumber = String(afterremain.dropFirst(2))
-    print(" номер : \(phnumber)")
+    let phoneNumber = String(phoneWithoutCode.dropFirst(2))
+    print(" номер : \(phoneNumber)")
 }
 phoneNumber(number: "375291061912")
 
@@ -135,30 +146,30 @@ func countFactorial( number: Int) -> Int {
     if number == 1 {
         return 1
     }
-      return number * countFactorial(number: number-1)
+      return number * countFactorial(number: number - 1)
 }
 print(countFactorial(number:5))
 
 //MARK: - 10
 // TODO: - пробел
-func printFib(index: Int){
+func printFib(index: Int) {
     if index <= 0 {
         return
     }
     // TODO: - отступы поехали. Надо подвинуть левее всю функцию
     // TODO: - пробелы
-        func fib( n: Int)-> Int{
-            if n == 0 {
-                return 0
+func fib(n: Int)-> Int{
+    if n == 0 {
+        return 0
             // TODO: - пробел
-            }else if n == 1 {
-                return 1
+    } else if n == 1 {
+        return 1
             }
-            return fib(n:n - 1 ) + fib(n:n - 2 )
+        return fib(n:n - 1 ) + fib(n:n - 2 )
         }
     for i in 0...index {
         // TODO: - пробел
-        print ( fib(n: i), terminator: i == index ? "\n" : ",")
+        print (fib(n: i), terminator: i == index ? "\n" : ",")
     }
 }
 printFib(index: 6)
@@ -170,7 +181,7 @@ func countSum(number: Int) -> Int {
         return 0
     }
     // TODO: - пробелы между математическими знаками
-    let sum = number/1000 + ((number/100) % 10) + ((number/10) % 10) + number % 10
+    let sum = number / 1000 + ((number / 100)  %  10) + ((number / 10) % 10) + number % 10
     return sum
 }
 print(countSum(number: 1234))
