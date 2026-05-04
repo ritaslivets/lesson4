17eaaf6379ac2b4947c804d16127c0a4ca001983
ritaslivets <rita4kasliva2007@gmail.com>
2026-05-04 22:00:59 +0300

    done2

--- home6.playground/Contents.swift
+++ home6.playground/Contents.swift
@@ -1,1 +1,3 @@
 typealias Student = (name: String, age: Int, grades: [String: Double], optionalSubjects: Set<String>)
+let students: [Student] = [("Misha", 19, ["Math": 8, "Physics": 6, "English": 4], ["Sports"]), ("Alice", 20, ["Math": 10, "English": 9, "History": 8], []), ("Max", 18, ["Physics": 9, "Cemistry": 7, "English": 6], ["Music", "Biology"]), ("Masha", 19, ["Math": 4, "Physics": 7, "English": 4], ["Art", "Musics"]), ("Alex", 21, ["Math": 7, "English":7, "History": 6],["Sports","Art"])]
+
